86be4b3b26723918dab22fe0ae74377abb8769c0
Jesper (Sung Hyun) Hong <78632105+Jesper-SH-Hong@users.noreply.github.com>
2024-09-06 10:29:38 -0700

    set hardhat gas, gasPrice for testing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,13 @@ import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    //to fix gas fee for awhile for testing.(still will be changed since ETH price is volatile)
+    hardhat: {
+      gas: 12000000,
+      gasPrice: 875000000,
+    },
+  }
 };
 
 export default config;
