81150a57904c19688a37abe20e7490deb2757a18
Jesper (Sung Hyun) Hong <78632105+Jesper-SH-Hong@users.noreply.github.com>
2024-09-12 16:15:18 -0700

    update comments

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -12,7 +12,9 @@ contract Factory {
     //returns: address of the paired ERC20
     function createExchange(address _token) public returns (address) {
 
-        //new Exchange => createExchange
+        //new Exchange로 새로운 Exchange 컨트랙트 배포.
+        //new로 호출 시 내부적으로 'create' OPcode => EVM creates new contract.
+        //cf) uniswap v2 'create2' => can get specific address by giving salt, etc.
         //Exchange 컨트랙트는 현재 디렉토리에 우리가 만든 것.
         Exchange exchange = new Exchange(_token);
         tokenToExchange[_token] = address(exchange);
