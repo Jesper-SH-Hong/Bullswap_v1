e6563116bdad3fc59cd9afdc0e7c6c56dbe03b02
Jesper (Sung Hyun) Hong <78632105+Jesper-SH-Hong@users.noreply.github.com>
2024-09-13 12:14:49 -0700

    [Hotfix] modify ethers method due to version downgrade

--- bullswap_client/src/utils/ethers.ts
+++ bullswap_client/src/utils/ethers.ts
@@ -1,7 +1,20 @@
 import { ethers } from "ethers";
+import { Factory__factory } from "../constants/typechain-types";
+import { FACTORY_ADDRESSES } from "../constants/addresses";
 
 export function getProvider() {
   //calling metamask a.k.a. window.ethereum
-  const provider = new ethers.BrowserProvider(window.ethereum);
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
   return provider;
+}
+
+export function getSigner() {
+  const provider = getProvider();
+  const signer = provider.getSigner();
+  return signer;
+}
+
+export function getFactoryContract(networkId: number) {
+  //use typechain 
+  return Factory__factory.connect(FACTORY_ADDRESSES[networkId], getSigner());
 }
