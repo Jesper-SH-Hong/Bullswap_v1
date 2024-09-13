473636d57a2e64d05b1537f0218e453bd9ca97e8
Jesper (Sung Hyun) Hong <78632105+Jesper-SH-Hong@users.noreply.github.com>
2024-09-13 10:04:29 -0700

    handleConnect, add injectedConnector to support certain network/chainID

--- bullswap_client/src/App.tsx
+++ bullswap_client/src/App.tsx
@@ -3,12 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import { useWeb3React } from '@web3-react/core';
 import { error } from 'console';
+import { injected } from './utils/connectors';
 
 
 //chainId: whch blockchain network the user is connected to mainnet / testnet?
 const { chainId, account, active, activate, deactivate } = useWeb3React();
 
-
+function handleConnect() {
+  if(active) {  //connected
+    deactivate();
+    return;
+  }
+  
+  //injected: which network to support?
+  activate(injected, (error) => {
+    if(error) {
+      alert(error);
+    }})
+}
 
 
 function App() {
---
+++ bullswap_client/src/utils/connectors.ts
@@ -0,0 +1,6 @@
+import {InjectedConnector } from '@web3-react/injected-connector'
+
+export const injected = new InjectedConnector({
+    //chainID  -> 1: mainnet, sepolia:11155111
+  supportedChainIds: [1, 11155111],
+})
