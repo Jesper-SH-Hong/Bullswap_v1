75f2422798f169d249f563d2022c1cf357a2e60c
Jesper (Sung Hyun) Hong <78632105+Jesper-SH-Hong@users.noreply.github.com>
2024-09-13 10:58:45 -0700

    prettier, bug fix

--- bullswap_client/src/App.tsx
+++ bullswap_client/src/App.tsx
@@ -1,39 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { useWeb3React } from '@web3-react/core';
-import { error } from 'console';
-import { injected } from './utils/connectors';
-
-
-//chainId: whch blockchain network the user is connected to mainnet / testnet?
-const { chainId, account, active, activate, deactivate } = useWeb3React();
-
-function handleConnect() {
-  if(active) {  //connected
-    deactivate();
-    return;
-  }
-  
-  //injected: which network to support?
-  activate(injected, (error) => {
-    if(error) {
-      alert(error);
-    }})
-}
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useWeb3React } from "@web3-react/core";
+import { error } from "console";
+import { injected } from "./utils/connectors";
 
 function App() {
-   return (
+  //chainId: whch blockchain network the user is connected to mainnet / testnet?
+  const { chainId, account, active, activate, deactivate } = useWeb3React();
+
+  function handleConnect() {
+    if (active) {
+      //connected
+      deactivate();
+      return;
+    }
+
+    //injected: which network to support?
+    activate(injected, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
+  }
+
+  return (
     <div className="App">
-       <div>
+      <div>
         <p>Account: {account}</p>
         <p>ChainId: {chainId}</p>
       </div>
       <div>
-        <button onClick={handleConnect}>{active ? 'DisConnect' : 'Connect'}</button>
+        <button onClick={handleConnect}>
+          {active ? "DisConnect" : "Connect"}
+        </button>
       </div>
-
     </div>
   );
 }
--- bullswap_client/src/utils/connectors.ts
+++ bullswap_client/src/utils/connectors.ts
@@ -1,6 +1,6 @@
-import {InjectedConnector } from '@web3-react/injected-connector'
+import { InjectedConnector } from "@web3-react/injected-connector";
 
 export const injected = new InjectedConnector({
-    //chainID  -> 1: mainnet, sepolia:11155111
+  //chainID  -> 1: mainnet, sepolia:11155111
   supportedChainIds: [1, 11155111],
-})
+});
