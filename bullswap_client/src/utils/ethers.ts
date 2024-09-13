35ab043681084cce3048dc178ffd011bfbe2ff04
Jesper (Sung Hyun) Hong <78632105+Jesper-SH-Hong@users.noreply.github.com>
2024-09-13 12:36:24 -0700

    connect Factory__factory

--- bullswap_client/src/utils/ethers.ts
+++ bullswap_client/src/utils/ethers.ts
@@ -15,6 +15,8 @@ export function getSigner() {
 }
 
 export function getFactoryContract(networkId: number) {
-  //use typechain 
+  //typechain-types로 컴파일해온 Factory__factory를 사용하여 Factory contract에 연결
+  //solidity의 contracts들은 typechain컴파일 시 __factory로 끝나는 파일이 생성되는듯.
+  //typechain의 connect는 solidity의 attach가 하던 것처럼 배포된 컨트랙트에 연결.
   return Factory__factory.connect(FACTORY_ADDRESSES[networkId], getSigner());
 }
